19c12b7a4a668527cde55bcecba8b841e34ff047
Augusto de Oliveira <engsoft.augustooliveira@gmail.com>
2025-07-14 17:18:03 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,12 @@
+var setaDireita = window.document.getElementById(seta-Direita)
+var Leonardo = window.document.getElementById(Leonardo)
+var Samantha = window.document.getElement(Samantha)
+var Bruna = window.document.getElementById(Bruna)
+var setaEsquerda = window.document.getElementById(seta-Esquerda)
+function RolaParaDireita() {
+    Leonardo.style = none 
+    Bruna.style = flex
+
+}
+
+
